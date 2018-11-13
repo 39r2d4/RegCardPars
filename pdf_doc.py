e3eaf262c2e10f8ede84f45e7faef121b98fc65a
39r2d4 <z67500@yandex.ru>
2018-11-13 12:36:51 +0200

    +исправлена проблема с поиском файлов -экспорт в csv глючит

--- pdf_doc.py
+++ pdf_doc.py
@@ -5,7 +5,8 @@
 import os
 import pickle
 from multiprocessing import Pool
-
+import re
+import csv
 import sys
 import argparse
 
@@ -17,6 +18,7 @@ from io import StringIO
 
 
 def convert_pdf_to_txt(path):
+    print(path)
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
@@ -41,8 +43,8 @@ def convert_pdf_to_txt(path):
     return text
 
 
-def find_file(suf, fdir):
-    filelist = []
+def find_file(suf, fdir, filelist = None):
+    if filelist is None: filelist = []
     names = os.listdir(fdir)
     for name in names:
         fullname = os.path.join(fdir, name)  # получаем полное имя
@@ -51,7 +53,7 @@ def find_file(suf, fdir):
             filelist.append(fullname)
         elif os.path.isdir(fullname):
             z = (os.path.abspath(fullname))
-            find_file(suf, z)
+            find_file(suf, z, filelist)
     return filelist
 
 
@@ -62,86 +64,61 @@ def pars_text(data):
     docIndex.append(len(data))
     # проверяем карточка или нет.
     if (data.find("КОНТРОЛЬНО-КАССОВОЙ ТЕХНИКИ")) == 0:
-        # print(x.find("КОНТРОЛЬНО-КАССОВОЙ ТЕХНИКИ"))
         # 1
         docIndex.append(data.find("Контрольно-кассовая техника"))
-        # print(x.find("Контрольно-кассовая техника"))
         # 2
         docIndex.append(data.find("Заводской номер экземпляра модели контрольно-кассовой техники:"))
-        # print(x.find("Заводской номер экземпляра модели контрольно-кассовой техники:"))
         # 3
         docIndex.append(data.find("регистрационный номер контрольно-кассовой техники:"))
-        # print(x.find("регистрационный номер контрольно-кассовой техники:"))
         # 4
         docIndex.append(data.find("Модель фискального накопителя:"))
-        # print(x.find("Модель фискального накопителя:"))
         # 5
         docIndex.append(data.find("Заводской номер экземпляра модели фискального накопителя:"))
-        # print(x.find("Заводской номер экземпляра модели фискального накопителя:"))
         # 6
         # надо преверять на кого ркгистрация (проверять по огрнип + инн для ип и огрн + инн\кпп для всех остальных
         docIndex.append(data.find("Принадлежит:"))
-        # print(x.find("Принадлежит:"))
         # 7
         docIndex.append(data.find("Адрес установки (применения) контрольно-кассовой техники:"))
-        # print(x.find("Адрес установки (применения) контрольно-кассовой техники:"))
         # 8
         docIndex.append(data.find("Место установки (применения) контрольно-кассовой техники:"))
-        # print(x.find("Место установки (применения) контрольно-кассовой техники:"))
         # 9
         docIndex.append(data.find("Входит в состав автоматического устройства для расчетов (да/нет)"))
-        # print(x.find("Входит в состав автоматического устройства для расчетов (да/нет)"))
         # 10
         docIndex.append(data.find("Обработку  фискальных  данных  осуществляет"))
-        # print(x.find("Обработку  фискальных  данных  осуществляет"))
         # 11
         docIndex.append(data.find("Количество перерегистраций контрольно-кассовой техники"))
-        # print(x.find("Количество перерегистраций контрольно-кассовой техники"))
         # 12
         docIndex.append(data.find("Зарегистрирована в налоговом органе с"))
-        # print(x.find("Зарегистрирована в налоговом органе с"))
 
-        ######################
-        # пока принтами вывод отладки, надо складывать в список, а список в еше один список.
-        # print(docIndex)
-
-        # что-то с индексами, надо писать сразу что и откуда берётся.
-
+        # циферки длина строки поиска + 1
         # модель ккт
-        # print(data[docIndex[1]+28:docIndex[2]-1]) #добавить проверку на спецсимволы типа \n и чистить от них
         docInfo.append(data[docIndex[1] + 28:docIndex[2] - 1])
         # серийный номер ккт
-        # print(data[docIndex[2]+63:docIndex[3]-1]) #убрать -1 от индека.
         docInfo.append(data[docIndex[2] + 63:docIndex[3] - 1])
-        # рег номер ккт
-        # print(data[docIndex[3]+51:docIndex[4]-1]) #наверное надо убрать пробелы
+        # рег номер ккт      #наверное надо убрать пробелы
         docInfo.append(data[docIndex[3] + 51:docIndex[4] - 1])
-        # модель ФН, думаю не шибко нужно
+        # модель ФН
+        docInfo.append(data[docIndex[4] + 31:docIndex[5] - 1])
         # ЗН ФН
-        # print(data[docIndex[5]+58:docIndex[6]-1])
         docInfo.append(data[docIndex[5] + 58:docIndex[6] - 1])
         # На кого зареган
-        # print(data[docIndex[6]+13:docIndex[7]-1])
         docInfo.append(data[docIndex[6] + 13:docIndex[7] - 1])
         # тут видимо надо будет вкорячить еще и инн кпп огрн и прочее
         # адрес установки ккт
-        # print(data[docIndex[7]+58:docIndex[8]-1]) #убрать \n
         docInfo.append(data[docIndex[7] + 58:docIndex[8] - 1])
         # место установки
-        # print(data[docIndex[8]+58:docIndex[9]-1])
         docInfo.append(data[docIndex[8] + 58:docIndex[9] - 1])
         # ОФД
-        # print(data[docIndex[10]+45:docIndex[11]-1])
         docInfo.append(data[docIndex[10] + 45:docIndex[11] - 1])
         # дата регистрации в налоговом органе
-        # print(data[docIndex[12]+40:docIndex[12]+50])
         docInfo.append(data[docIndex[12] + 40:docIndex[12] + 50])
+        # возможно есть способ быстрее
+        for x in range(len(docInfo)):
+            docInfo[x] = re.sub("^\s+|\n|\r|\s+$", '', docInfo[x])
         return docInfo
-
 
 def pars_filelist(fl):
     data = []
-    print(len(fl))
     for x in fl:
         # разбираем страничку и положим результат в список
         text = pars_text(convert_pdf_to_txt(x))
@@ -154,7 +131,6 @@ def pars_filelist(fl):
 
 def pars_filelist_map(fl):
     data = []
-    print(fl)
     # разбираем страничку и положим результат в список
     text = pars_text(convert_pdf_to_txt(fl))
     if type(text) == list:  # проверяем не пустой ли список
@@ -165,22 +141,34 @@ def pars_filelist_map(fl):
 
 # pickle
 def record_data_in_file(data, file):
-    with open(file, 'wb') as f:
-        pickle.dump(data, f)
+    with open(file, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter='|',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['модель ккт', 'серийный номер ккт', 'рег номер ккт',
+                             'модель ФН', 'ЗН ФН', 'На кого зареган', 'адрес установки ккт',
+                             'место установки', 'ОФД', 'дата регистрации в налоговом органе',
+                             'расположение файла'])
+        for x in data:
+            if len(x) != 0:
+                print(x)
+                spamwriter.writerow(x)
 
 
 
-def main_wiz(dir: str, mode: int = 1, out: str = 'data.pl'):
+def main_wiz(dir: str, mode: int = 1, out: str = 'data.csv'):
     # mode: 1 - 1 thread, 2 - 2 thread, 4 - 4 thread
     # out: output file
+    print('!scan folder!')
     filelist = find_file('pdf', dir)
     if mode > 1:
         pool = Pool(mode)
+        print('!pars file!')
         pdfdata = pool.map(pars_filelist_map, filelist)
         pool.close()
         pool.join()
         record_data_in_file(pdfdata, out)
     else:
+        print('!pars file!')
         pdfdata = pars_filelist(filelist)
         record_data_in_file(pdfdata, out)
 
@@ -189,7 +177,7 @@ def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dir', type=str, default='.')
     parser.add_argument('-m', '--mode', type=int, default=1)
-    parser.add_argument('-o', '--out', type=str, default='data.pl')
+    parser.add_argument('-o', '--out', type=str, default='data.csv')
     return parser
 
 
@@ -200,3 +188,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    #main_wiz('./folder', 3)
